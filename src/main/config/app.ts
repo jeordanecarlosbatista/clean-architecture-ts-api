81b90fb0f0a5c7fec5fc3386e4fd68e30d6d732f
jeordanecarlosbatista <jeordane.batista@hotmail.com>
2021-10-16 09:29:12 -0300

    Refactoring scripts configuration

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
 import express, { Express } from 'express';
+import setupStaticFiles from '@main/config/static-files';
 import setupSwagger from './swagger';
 import setupRoutes from './routes';
 import setupMiddlewares from './middlewares';
 
 export const setupApp = async (): Promise<Express> => {
   const app = express();
+  setupStaticFiles(app);
   setupSwagger(app);
   setupMiddlewares(app);
   setupRoutes(app);
--- src/main/server.ts
+++ src/main/server.ts
@@ -5,6 +5,6 @@ MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const { setupApp } = await import('./config/app');
     const app = await setupApp();
-    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port} asd`));
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`));
   })
   .catch(console.error);
