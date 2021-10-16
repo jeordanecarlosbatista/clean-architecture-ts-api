6027310c46de5705966db8b9b8464c2e1ff72ef1
jeordanecarlosbatista <jeordane.batista@hotmail.com>
2021-10-16 10:16:45 -0300

    Add config mongodb

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   port: process.env.PORT || 3333,
   jwtSecret: process.env.JWT_SECRET || 'abcd@123',
-  mongoUrl: process.env.MONGO_URL || 'mongodb://192.168.1.105:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
 };
