44c65a074dd747443348b77e5cf15462a9d65180
jeordanecarlosbatista <jeordane.batista@hotmail.com>
2021-10-17 11:28:47 -0300

    Ajustes sonar

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -2,12 +2,16 @@ import { Express, Router } from 'express';
 import { readdirSync } from 'fs';
 import { join } from 'path';
 
-export default (app: Express): void => {
+export default async (app: Express): Promise<void> => {
   const router = Router();
   app.use('/api', router);
-  readdirSync(join(__dirname, '../routes')).map(async (file) => {
+
+  const routes = readdirSync(join(__dirname, '../routes'));
+
+  for (const file of routes) {
     if (!file.endsWith('.map')) {
+      // eslint-disable-next-line no-await-in-loop
       (await import(`../routes/${file}`)).default(router);
     }
-  });
+  }
 };
