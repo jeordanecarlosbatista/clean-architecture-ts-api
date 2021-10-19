a5db12aac1b09c441934d1cc802e6a87b5cb439b
jeordanecarlosbatista <jeordane.batista@hotmail.com>
2021-10-19 15:28:55 -0300

    teste

--- src/main/factories/decorators/log-controller-decorator-factory.ts
+++ src/main/factories/decorators/log-controller-decorator-factory.ts
@@ -4,5 +4,6 @@ import { Controller } from '@presentation/protocols';
 
 export const makeLogControllerDecorator = (controller: Controller): Controller => {
   const logMongoRepository = new LogMongoRepository();
+  console.log('teste');
   return new LogControllerDecorator(controller, logMongoRepository);
 };
