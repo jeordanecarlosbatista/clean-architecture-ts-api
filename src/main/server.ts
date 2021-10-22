3e5a13bf3b72172691ad37a4aaecf9f3d192653d
jeordanecarlosbatista <jeordane.batista@hotmail.com>
2021-10-22 17:45:23 -0300

    Teste

--- src/main/server.ts
+++ src/main/server.ts
@@ -5,6 +5,6 @@ MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const { setupApp } = await import('./config/app');
     const app = await setupApp();
-    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`));
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port} teste`));
   })
   .catch(console.error);
